65b025d257795b188074efecfd56388a0ddb77e0
Ondrej Sika <ondrej@ondrejsika.com>
2020-11-30 17:40:36 +0100

    Add static scrape config for prom

--- run-services.sh
+++ run-services.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose up -d --force-recreate service-0 service-1 service-2 service-3
+docker-compose up -d --force-recreate service-0 service-1 service-2 service-3 service-static-1 service-static-2
